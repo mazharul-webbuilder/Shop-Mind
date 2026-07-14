f59c2c155d050b07f12885c55217499148b9c031
Mazharul <mazharul.jatri@gmail.com>
2026-07-14 18:01:26 +0600

    frontend structure

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Frontend\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::name('frontend.')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('home');
 });
