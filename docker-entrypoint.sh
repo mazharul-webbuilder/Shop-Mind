b1342a2d78356a34d8a874d3152886188ed39323
Mazharul Islam <58305538+mazharul-webbuilder@users.noreply.github.com>
2026-07-16 15:24:29 +0600

    mod

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,12 @@ a2dismod mpm_worker 2>/dev/null || true
 # Force only PHP's module to run
 a2enmod mpm_prefork 2>/dev/null || true
 
+# Enable Apache header module (needed to handle proxy headers)
+a2enmod headers 2>/dev/null || true
+
+# Force Apache to tell Laravel that the traffic came in securely over HTTPS
+echo "RequestHeader set X-Forwarded-Proto 'https'" >> /etc/apache2/apache2.conf
+
 # Point Apache to Railway's assigned port (defaults to 80 if not set)
 PORT="${PORT:-80}"
 sed -i "s/Listen 80/Listen ${PORT}/g" /etc/apache2/ports.conf
